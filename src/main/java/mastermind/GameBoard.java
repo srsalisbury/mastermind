321c4f49f62ba0a9250c72a138c3ef8e21395d8d
Steven Salisbury <steven@salisburyclan.com>
2019-12-02 21:19:24 -0800

    First checkin.

---
+++ src/main/java/mastermind/GameBoard.java
@@ -0,0 +1,26 @@
+package mastermind;
+
+public class GameBoard {
+
+  //	private final PieceSet solution;
+  //	private List<Turn> turns;
+
+  public GameBoard() {
+    // constructor
+  }
+  /*
+  public void addTurn(Turn turn) {
+
+  }
+
+  public List<Turn> getTurns() {
+
+  }
+
+  public boolean hasWon() {
+  	for (Turn turn : turns) {
+  		if ()
+  	}
+  }
+  */
+}
---
+++ src/main/java/mastermind/Turn.java
@@ -0,0 +1,19 @@
+package mastermind;
+
+import com.google.auto.value.AutoValue;
+
+@AutoValue
+public abstract class Turn {
+
+  public static Turn create(PieceSet guess, Result result) {
+    return new AutoValue_Turn(guess, result);
+  }
+
+  public abstract PieceSet guess();
+
+  public abstract Result result();
+
+  public boolean hasWon() {
+    return result().hasWon();
+  }
+}
